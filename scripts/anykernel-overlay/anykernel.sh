33216cd7f25a5904b743e8675dac16454ead6110
thirteenth13 <31848255+thirteenth13@users.noreply.github.com>
2026-08-18 14:10:28 +0300

    build: add deterministic AnyKernel metadata overlay

---
+++ scripts/anykernel-overlay/anykernel.sh
@@ -0,0 +1,40 @@
+# Deterministic metadata overlay for the pinned Kurozuka97 vayu/bhima AnyKernel3 tree.
+# The source tree supplies the AnyKernel implementation; this file only supplies
+# project metadata required by the release package.
+# AnyKernel3 Ramdisk Mod Script
+# osm0sis @ xda-developers
+
+## AnyKernel setup
+# begin properties
+properties() { '
+kernel.string=KyriePatch
+do.devicecheck=1
+do.modules=0
+do.systemless=1
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=vayu
+device.name2=bhima
+device.name3=
+device.name4=
+device.name5=
+supported.versions=11 - 17
+supported.patchlevels=
+'; } # end properties
+
+# shell variables
+block=/dev/block/bootdevice/by-name/boot;
+is_slot_device=0;
+ramdisk_compression=auto;
+
+## AnyKernel methods (DO NOT CHANGE)
+. tools/ak3-core.sh;
+
+## AnyKernel file attributes
+set_perm_recursive 0 0 755 644 $ramdisk/*;
+set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
+
+## AnyKernel boot install
+dump_boot;
+write_boot;
+## end boot install
