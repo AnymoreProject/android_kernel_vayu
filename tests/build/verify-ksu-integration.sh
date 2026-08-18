555990708842bd04fa361cbc27353af0e8460a31
thirteenth13 <31848255+thirteenth13@users.noreply.github.com>
2026-08-18 16:11:01 +0300

    test: require SuSFS credential type declaration

--- tests/build/verify-ksu-integration.sh
+++ tests/build/verify-ksu-integration.sh
@@ -135,6 +135,7 @@ fi
 
 require_sha256 "fs/susfs.c" 3d3462246f981b411c93ed4144f3575c046ef55ae1d1b81db5c1bcb6b5591082
 require_sha256 "include/linux/susfs.h" 05d4ec96ba75d459612d6269614bc7e1948c4e7b1ecd4dfaf47fbd4ec4a3fcfb
+require_contains "include/linux/susfs_def.h" "#include <linux/cred.h>"
 require_contains "include/linux/susfs_def.h" "__kuid_val(current_uid()) >= 10000);"
 require_contains "patches/KernelSU-Next/0001-susfs-2.2.0.patch" \
   "typedef asmlinkage long (*syscall_fn_t)(const struct pt_regs *regs);"
