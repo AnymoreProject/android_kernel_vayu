eecf96ea88a72548319ed7c73e538dd7350d3b18
thirteenth13 <31848255+thirteenth13@users.noreply.github.com>
2026-08-18 13:28:57 +0300

    test: require 4.14 fsnotify mark ownership

--- tests/build/verify-ksu-integration.sh
+++ tests/build/verify-ksu-integration.sh
@@ -147,6 +147,10 @@ require_contains "fs/susfs.c" \
 require_contains "fs/susfs.c" \
   ".free_mark = susfs_free_sdcard_mark,"
 require_contains "fs/susfs.c" \
+  "static int add_mark_on_inode(struct inode *inode, u32 mask);"
+require_contains "fs/susfs.c" \
+  "ret = add_mark_on_inode(wd->inode, wd->mask);"
+require_contains "fs/susfs.c" \
   "fsnotify_add_mark(m, inode, NULL, 0)"
 if grep -Fq -- ".handle_inode_event" "$ROOT_DIR/fs/susfs.c" ||
    grep -Fq -- "fsnotify_add_inode_mark" "$ROOT_DIR/fs/susfs.c"; then
